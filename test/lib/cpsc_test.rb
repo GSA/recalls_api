9cf623db52f2fa34b5b6eb3fb5d00123697b7686
Aidan Feldman <aidan.feldman@gsa.gov>
2018-09-25 21:32:06 -0400

    refactor stubbing in tests

--- test/lib/cpsc_test.rb
+++ test/lib/cpsc_test.rb
@@ -1,18 +1,22 @@
 require 'test_helper'
 
 class CPSCTest < ActiveSupport::TestCase
+  def stub_request(response)
+    Typhoeus.stub(CPSC::URL).and_return(response)
+  end
+
   test "returns the data" do
     fixture = File.expand_path('../../fixtures/files/cpsc.json', __FILE__)
     data = File.read(fixture)
     response = Typhoeus::Response.new(code: 200, body: data.to_json)
-    Typhoeus.stub(CPSC::URL).and_return(response)
+    stub_request(response)
 
     assert_equal data, CPSC.get_recalls
   end
 
   test "raises an exception on a failure" do
     response = Typhoeus::Response.new(code: 500)
-    Typhoeus.stub(CPSC::URL).and_return(response)
+    stub_request(response)
 
     assert_raises do
       CPSC.get_recalls
