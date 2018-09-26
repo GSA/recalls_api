cbc649e9152c9e77aeb018ecc5f5e8da9a48c9ce
Aidan Feldman <aidan.feldman@gsa.gov>
2018-09-25 21:29:57 -0400

    add test for CPSC failure case

--- test/lib/cpsc_test.rb
+++ test/lib/cpsc_test.rb
@@ -9,4 +9,13 @@ class CPSCTest < ActiveSupport::TestCase
 
     assert_equal data, CPSC.get_recalls
   end
+
+  test "raises an exception on a failure" do
+    response = Typhoeus::Response.new(code: 500)
+    Typhoeus.stub(CPSC::URL).and_return(response)
+
+    assert_raises do
+      CPSC.get_recalls
+    end
+  end
 end
