fc7e6f660877720d9a73d91bd13951cf235c64f6
Aidan Feldman <aidan.feldman@gsa.gov>
2018-09-26 01:27:51 -0500

    configure VCR to work with rspec

--- spec/importers/cpsc_spec.rb
+++ spec/importers/cpsc_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 require 'vcr_helper'
 
-RSpec.describe CPSC do
+RSpec.describe CPSC, :vcr do
   def stub_request(response)
     Typhoeus.stub(CPSC::URL).and_return(response)
   end
 
   it "returns the data" do
-    VCR.use_cassette('cpsc') do
-      data = CPSC.get_recalls(RecallDescription: 'BlueStar')
-      expect(data.size).to eq(3)
-    end
+    data = CPSC.get_recalls(RecallDescription: 'BlueStar')
+    expect(data.size).to eq(3)
   end
 
   it "raises an exception on a failure" do
--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -1,4 +1,5 @@
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :typhoeus
+  config.configure_rspec_metadata!
 end
